c04517d6c6c1497faa6604a0a359491c5e6f38e2
carvalhoandre <andre_carvalho0@live.com>
2024-09-02 21:09:14 -0300

    feat: update tsconfig and config slint

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,4 @@
-import IComponent from "../../@types";
+import IComponent from "src/@types";
 
 const Home: IComponent = ({ testId = "home-page" }) => {
   return <h1 data-testId={testId}>Home</h1>;
