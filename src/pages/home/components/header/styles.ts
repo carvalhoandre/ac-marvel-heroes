5f029172d85b0fd4a2b0b949d3a84acab7dcdcc5
carvalhoandre <andre_carvalho0@live.com>
2024-09-02 21:19:35 -0300

    feat: header

---
+++ src/pages/home/components/header/styles.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+`;
+
+export const Title = styled.h1`
+  font-size: 3rem;
+  font-weight: 900;
+  text-align: center;
+  text-transform: uppercase;
+`;
+
+export const SubTitle = styled.p`
+  font-size: 1.5rem;
+  font-weight: 500;
+  text-align: center;
+`;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,15 @@
 import IComponent from "src/@types";
 
+import { Header } from "./components/header";
+
+import { Container } from "./styles";
+
 const Home: IComponent = ({ testId = "home-page" }) => {
-  return <h1 data-testId={testId}>Home</h1>;
+  return (
+    <Container data-testid={`${testId}-container`}>
+      <Header />
+    </Container>
+  );
 };
 
 export { Home };
