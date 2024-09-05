45f0dc554a8a11bb9c34c6d0abce3ccf0d9b1339
carvalhoandre <andre_carvalho0@live.com>
2024-09-05 12:55:34 -0300

    feat: footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import { Home } from "./pages/home";
 import "./App.css";
+import { Footer } from "@components/footer";
 
 function App() {
-  return <Home />;
+  return (
+    <>
+      <Home />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
